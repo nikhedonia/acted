163cecc56861c663d0ecb584d63543e4dacf1160
Gaetano Checinski <Gaetano.Checinski@gmail.com>
2017-03-29 17:18:21 +0100

    bugfixes; renames timewindowed to timeframed

--- index.js
+++ index.js
@@ -1,37 +1,46 @@
 let co = require('co');
 
 const shift = q => q.shift();
+const last = q => {
+  const x = q.slice(-1)[0];
+  q=[];
+  return x;
+};
+
 const actor = (act, pick = shift) => {
   let queue = [];
   let notify = ()=>{};
 
-  let next = () => {
+  let next = (sizzle) => {
+
+    if(sizzle) q = sizzle(q);
+
     if(queue.length) {
       notify = () => {};
       return Promise
         .resolve(pick(queue));
     }
+
     return new Promise(done=>{notify = done})
       .then(_=>pick(queue));
   }
 
   co(function*(){
-      yield* act(next);
-  });
+    yield* act(next);
+  }).catch(e => console
+    .log('actor terminated due to unhandled exception: ', e)
+  )
 
-  return (task) => {
+
+  return msg => {
     let D, R;
 
-    let p = new Promise((done, resolve) => {
-      D=done;
-      R=resolve;
+    let p = new Promise((done, reject) => {
+      D = done;
+      R = reject;
     });
 
-    queue.push(() => Promise
-      .resolve(task())
-      .then(D)
-      .catch(R)
-    );
+    queue.push([msg, D, R]);
 
     notify();
     return p;
@@ -40,13 +49,54 @@ const actor = (act, pick = shift) => {
 
 const wait = (ms, x) => new Promise(d => setTimeout(_=>d(x), ms));
 
-const timeWindowed = (work, ms) =>
-  x => work(_=>Promise.all([x(), wait(ms)]))
-    .then(x=>x[0])
+
+
+const callIT = x=>{
+  if(typeof(x) == 'function') return x();
+  return x;
+}
+
+const tryResolve = p => new Promise( done => {
+  Promise
+    .resolve(p)
+    .then( x=> done([1,x]) )
+    .catch( x=> done([0,x]) )
+})
+
+const each = (f) => {
+  return function*(next) {
+    while(1) {
+      const [msg, done, reject] = yield next();
+      const [success, result] = yield tryResolve(f(msg));
+      const Next = success?done:reject;
+
+      Next((result!=undefined)?
+        result:
+        msg
+      );
+    }
+  }
+}
+
+const timeFramed = (ms, act) => actor(function*(next){
+  while(1) {
+    const [value, done, error] = yield next();
+    const watch = wait(ms);
+    const task = act(value);
+
+    task.then(done)
+      .catch(error);
+
+    yield [task, watch];
+  }
+})
 
 function* worker(next) {
   while(1) {
-    yield (yield next())()
+    const [task, done, reject] = yield next();
+    const [success, result] = yield tryResolve(callIT(task));
+    const Next = success ? done: reject;
+    Next(result);
   }
 }
 
@@ -54,7 +104,9 @@ function* worker(next) {
 module.exports = {
   worker,
   wait,
-  timeWindowed,
+  each,
+  tryResolve,
+  timeFramed,
   actor,
   shift
 }
